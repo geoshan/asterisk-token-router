1764d744761a4bfdfed825bde4bbf2b561a60acf
daojun <daojun@asterisk-token-router.dev>
2026-08-08 20:58:10 +0800

    fix: UpdateAbilities用事务防竞态+1062重复键

--- model/ability.go
+++ model/ability.go
@@ -74,21 +74,30 @@ func (channel *Channel) DeleteAbilities() error {
 	return DB.Where("channel_id = ?", channel.Id).Delete(&Ability{}).Error
 }
 
-// UpdateAbilities updates abilities of this channel.
-// Make sure the channel is completed before calling this function.
 func (channel *Channel) UpdateAbilities() error {
-	// A quick and dirty way to update abilities
-	// First delete all abilities of this channel
-	err := channel.DeleteAbilities()
-	if err != nil {
-		return err
-	}
-	// Then add new abilities
-	err = channel.AddAbilities()
-	if err != nil {
-		return err
-	}
-	return nil
+	// Use transaction to prevent race between delete and add
+	return DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Where("channel_id = ?", channel.Id).Delete(&Ability{}).Error; err != nil {
+			return err
+		}
+		models_ := strings.Split(channel.Models, ",")
+		models_ = utils.DeDuplication(models_)
+		groups_ := strings.Split(channel.Group, ",")
+		abilities := make([]Ability, 0, len(models_))
+		for _, model := range models_ {
+			for _, group := range groups_ {
+				ability := Ability{
+					Group:     group,
+					Model:     model,
+					ChannelId: channel.Id,
+					Enabled:   channel.Status == ChannelStatusEnabled,
+					Priority:  channel.Priority,
+				}
+				abilities = append(abilities, ability)
+			}
+		}
+		return tx.Create(&abilities).Error
+	})
 }
 
 func UpdateAbilityStatus(channelId int, status bool) error {
